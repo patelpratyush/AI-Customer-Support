a3b27223d2c5de3e6e3f3832bf629389e1430217
patelpratyush <patelpratyush28@gmail.com>
2024-08-08 15:00:25 -0400

    update user-auth branch

--- firebase.js
+++ firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyALoZ1udaj7iCoRkGTYcSdzmPU9ONNaW7A",
-  authDomain: "ai-customer-support-651b8.firebaseapp.com",
-  projectId: "ai-customer-support-651b8",
-  storageBucket: "ai-customer-support-651b8.appspot.com",
-  messagingSenderId: "447368286114",
-  appId: "1:447368286114:web:21794c79ad46143cf46cfb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
